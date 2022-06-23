dff3ec41979edaba02ab36410fda94c67e2cf039
+ <+>
2022-06-23 01:12:35 -0400

    fixed styling and updated dropdown and tags

--- menutodetails.js
+++ menutodetails.js
@@ -1,8 +1,16 @@
+//elements regarding menu and details div
 const innerMenuDiv = document.getElementById("inner-menu-div");
-
 const detailsImage = document.getElementById("display-image");
 const detailsDiv = document.getElementById("display-classic-drink");
 const detailImageContainer = document.getElementById("drink-details");
+const detailTitle = document.getElementById("details-title");
+
+
+//elements regarding dropdown/submit 
+const dropDown = document.getElementById("edit-drink-dropdown"); //entire submit form
+const ingredientListResults = document.getElementById("autocomplete-list");
+const ingredientInput = document.getElementById("add-ingredient");
+const buttonsDiv = document.getElementById("ingredients-buttons");
 
 
 
@@ -13,7 +21,6 @@ fetch("http://localhost:3000/drinks")
     createDrinksMenu(drinksArray);
     populateDetailsfromMenu(drinksArray[0]);
     
-
 });
 
 function createDrinksMenu(drinksArray) {
@@ -29,28 +36,89 @@ function createDrinksMenu(drinksArray) {
     });
 }
 
-
 function populateDetailsfromMenu(drink) {
+    detailTitle.textContent = drink.name
     detailsImage.src = drink.image
     detailsImage.alt = drink.name
     detailImageContainer.append(detailsImage);
+    buttonsDiv.innerHTML = ""
     drink.ingredients.forEach(ingredient => {
+        console.log(ingredient)
         const existingIngredient = document.createElement("span")
         existingIngredient.className = "ingredient-tags"
-        existingIngredient = ingredient
+        existingIngredient.textContent = ingredient
         buttonsDiv.append(existingIngredient);
 
-<<<<<<< HEAD
-};
-=======
     })
    
-
 };
 
 
 
+fetch("http://localhost:3000/ingredients")
+.then(res => res.json())
+.then(ingredientsArray => {
+    // populateDropdown(ingredientsArray);
+    createDropDownIngredients(ingredientsArray)
+});
 
+
+
+
+function createDropDownIngredients(ingredientsArray) {
+    // console.log(ingredientsArray)
+    ingredientInput.addEventListener("keyup", (e) => {
+        console.log(e.target.value);
+        let results = [];
+        let input = ingredientInput.value;
+        if (input.length) {
+            results = ingredientsArray.filter((ingredient) => {
+            return ingredient.toLowerCase().includes(input.toLowerCase())
+        });
+        }
+        populateDropdown(results);
+    });
+    };
+
+
+
+function populateDropdown(results) {
+    if (!results.length) {
+        return ingredientListResults.classList.remove("show"); 
+    } 
+    let content = results.map((ingredient) => {
+    return `<li>${ingredient}</li>`}).join("");
+
+    ingredientListResults.innerHTML = `<ul>${content}</ul>`;
+    // ingredientForButton = `${content}`
+    console.log(ingredientListResults)
+    console.log(content)
+    console.log(results);
+    ingredientListResults.classList.add("show");
+
+    
+};
+
+ingredientListResults.addEventListener("click", (e) => {
+    console.log(e.target.textContent);
+    const ingredientTag = document.createElement("span");
+    const removeButton = document.createElement("button");
+    removeButton.textContent = "x"
+    removeButton.className = "remove-tag"
+    removeButton.addEventListener("click", () => {
+        removeButton.parentNode.remove();
+    });
+    ingredientTag.textContent = e.target.textContent.toLowerCase();
+    ingredientTag.className = "ingredient-tags"
+    ingredientTag.append(removeButton);
+    buttonsDiv.append(ingredientTag);
+
+})
+
+// dropDown.addEventListener("submit", (e) => {
+
+// })
+//TO DO - SUBMIT INGREDIENT TAGS, POST TO JSON 
 
 
 
@@ -68,4 +136,3 @@ function populateDetailsfromMenu(drink) {
 //     }
 
 // };
->>>>>>> fbfb8a206c20ca59749d49e8aafbd2c1505eb059
